302ae2a96ed2138dbf3dd296f2328c8f2e656a2c
Mat Moore <MatMoore@users.noreply.github.com>
2025-03-20 13:52:26 +0000

    Initial commit

---
+++ test_all.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+
+env = Environment(loader=FileSystemLoader("templates"), autoescape=select_autoescape())
+
+
+def test_checkboxes():
+    template = env.get_template("test/checkbox/checkbox.jinja")
+    html = template.render()
+    html = ""
+    with (Path(__name__).parent / "templates/test/checkbox/checkbox.html").open() as f:
+        expected = f.read()
+
+    assert html == expected
